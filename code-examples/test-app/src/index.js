7bd547e5df2ec82757bea9bb4ed4af790f9ce452
Mark D Christensen <mark@brooksnet.com>
2023-06-29 16:33:51 -0600

    Added bootstrap.js

--- code-examples/test-app/src/index.js
+++ code-examples/test-app/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './App';
+import './App.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+// import App from './App';
 import Page from './components/page';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
